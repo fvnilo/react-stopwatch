d19a73ec03eff8feec817c878a034b062f286b9b
Ny Fanilo Andrianjafy <nandrianjafy@tritondigital.com>
2016-07-08 16:44:24 -0400

    Created a mobx store factory

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -6,9 +6,9 @@ const StopWatch = ({ timer }) => (<div>
     <h1>
       Time Remaining: { timer.timeRemaining }
     </h1>
-     <button onClick={timer.start}>Start</button>
-     <button onClick={timer.stop}>Stop</button>
-     <button onClick={timer.reset}>Reset</button>
+     <button onClick={() => timer.start()}>Start</button>
+     <button onClick={() => timer.stop()}>Stop</button>
+     <button onClick={() => timer.reset()}>Reset</button>
   </div>);
 
 
--- src/store.js
+++ src/store.js
@@ -1,47 +1,40 @@
- import mobx from 'mobx';
+import { createMobxStore } from 'utils';
 
- function createStore() {
-   let intervalToken = null;
+const tick = store => {
+  store.timeRemaining--;
 
-   const tick = store => {
-     store.timeRemaining--;
+  if (store.timeRemaining === 0) {
+    store.stop();
+  }
+};
 
-     if (store.timeRemaining === 0) {
-       store.stop();
-     }
-   };
+export default function storeFactory() {
+  let intervalToken = null;
 
-   let store = {
-     timeRemaining: null,
+  return createMobxStore(
+    {
+      timeRemaining: 10
+    },
+    {
+      stop() {
+        if (intervalToken) {
+          clearInterval(intervalToken);
+          intervalToken = null;
+        }
+      },
 
-     stop() {
-       if (intervalToken) {
-         clearInterval(intervalToken);
-         intervalToken = null;
-       }
-     },
+      start() {
+        let self = this;
 
-     start() {
-       if (!intervalToken) {
-         intervalToken = setInterval(function() {
-           tick(store);
-         }, 1000);
-       }
-     },
+        if (!intervalToken) {
+          intervalToken = setInterval(function() {
+            tick(self);
+          }, 1000);
+        }
+      },
 
-     reset() {
-       store.timeRemaining = 10;
-     }
-   };
-
-   return store;
- }
-
- export default function storeFactory() {
-   let store = createStore();
-   let observables = { timeRemaining: 10 };
-
-   mobx.extendObservable(store, observables);
-
-   return store;
- }
+      reset() {
+        this.timeRemaining = 10;
+      }
+    });
+}
